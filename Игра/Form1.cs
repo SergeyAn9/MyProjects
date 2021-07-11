0af23e6f0e315a053bec686dc50afbdcf5c5d5d3
SergeyAn9 <79638618609@mail.ru>
2021-07-11 19:10:31 +0500

    1#Commit

---
+++ Игра/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Игра
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap Krug1Texure = Resource2.krug,
+                      Krug2Texure = Resource2.krug1;        
+        private Point _targetPosition = new Point(300, 300);
+        private Point _direction = Point.Empty;
+        private int _score = 0;
+        public Form1()
+        {
+            InitializeComponent();           
+            SetStyle(ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint, true);
+
+            UpdateStyles();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            timer2.Interval = r.Next(20, 300);
+            _direction.X = r.Next(-1, 2);
+            _direction.Y = r.Next(-1, 2);
+
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            var localPosition = this.PointToClient(Cursor.Position);
+
+            _targetPosition.X += _direction.X * 20;
+            _targetPosition.Y += _direction.Y * 20;
+
+            if(_targetPosition.X < 10 || _targetPosition.X > 1000)
+            {
+                _direction.X *= -1;
+            }
+            if(_targetPosition.Y < 10 || _targetPosition.Y > 600)
+            {
+                _direction.Y *= -1;
+            }
+
+            Point between = new Point(localPosition.X - _targetPosition.X, localPosition.Y - _targetPosition.Y);
+            float distance = (float)Math.Sqrt((between.X * between.X) + (between.Y * between.X));
+
+            if(distance < 10)
+            {
+                AddScore(1);
+                if (_score == 50)
+                {
+                    Over("Over");
+                }
+            }
+
+            var krugRect = new Rectangle(localPosition.X - 50, localPosition.Y - 50, 100, 100);
+            var krug1Rect = new Rectangle(_targetPosition.X - 50, _targetPosition.Y - 50, 100, 100);
+
+            g.DrawImage(Krug1Texure, krugRect);
+            g.DrawImage(Krug2Texure, krug1Rect);
+        }
+        private void Over(string str)
+        {
+            label1.Text = str;
+            label1.Location = new Point(500, 50);
+            label1.Visible = true;
+            timer1.Stop();
+            timer2.Stop();
+        }
+        private void AddScore(int score)
+        {
+            _score += score;
+            scoreLabel.Text = _score.ToString(); 
+        }
+    }
+}
---
+++ Игра/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Игра")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Игра")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("a12d6dcd-7d54-460b-9fe5-3d71445f153d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
